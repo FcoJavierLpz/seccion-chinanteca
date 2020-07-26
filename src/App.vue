5659e6e37c834a683494bfb0565ceb6a7ef7aeac
Francisco J <flopez@linko.mx>
2020-07-26 14:38:38 -0500

    adding footer

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,14 @@
 <script>
 import Header from '@/components/Header.vue';
 import SectionDirectory from '@/components/Section-Directory.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
     SectionDirectory,
+    Footer,
   },
 };
 </script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>
-    Footer
-  </div>
+  <footer class="footer">
+    <div class="footer__logo-box">
+      <picture class="footer__logo">
+        <source
+          srcset="img/logo-green-small-1x.png 1x, img/logo-green-small-2x.png 2x"
+          media="(max-width: 37.5em)"
+        />
+        <img
+          srcset="@/assets/img/icon-logo-2.png 1x, @/assets/img/icon-logo-2.png 2x"
+          alt="Full logo"
+          src="@/assets/img/icon-logo-2.png"
+        />
+      </picture>
+    </div>
+  </footer>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 <style lang="scss" scoped>
+.footer {
+  background-color: $color-grey-dark-3;
+  padding: 10rem 0;
+  font-size: 1.4rem;
+  color: $color-grey-light-1;
 
+  @include respond(tab-port) {
+    padding: 8rem 0;
+  }
+
+  &__logo-box {
+    text-align: center;
+    margin-bottom: 8rem;
+
+    @include respond(tab-port) {
+      margin-bottom: 6rem;
+    }
+  }
+
+  &__logo {
+    width: 15rem;
+    height: auto;
+  }
+}
 </style>
