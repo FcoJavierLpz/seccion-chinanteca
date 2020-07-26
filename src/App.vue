4fdbece020f70752691eec756a083fe0f185c410
Francisco J <flopez@linko.mx>
2020-07-26 14:11:45 -0500

    adding section for directory

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
     <Header></Header>
+    <main>
+      <section-directory></section-directory>
+    </main>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue';
+import SectionDirectory from '@/components/Section-Directory.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
+    SectionDirectory,
   },
 };
 </script>
---
+++ src/components/Section-Directory.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section-directory" id="directory">
+    <div class="u-center-text u-margin-bottom-big">
+      <h2 class="heading-secondary">
+        Directorio
+      </h2>
+    </div>
+    <div class="row">
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionDirectory',
+};
+</script>
+
+<style lang="scss" scoped></style>
