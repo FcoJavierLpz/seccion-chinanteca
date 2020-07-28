08317d188b271bb044daef65c679f33bb00310b2
Francisco J <flopez@linko.mx>
2020-07-28 13:15:45 -0500

    adding author in the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,14 +2,17 @@
   <footer class="footer">
     <div class="footer__logo-box">
       <picture class="footer__logo">
-        <img
-          alt="logo verde"
-          src="@/assets/img/logo-green.svg"
-        />
+        <img alt="logo verde" src="@/assets/img/logo-green.svg" />
       </picture>
-      <div class="footer__copyright">
-         © Copyright 2020
-      </div>
+      <p class="footer__copyright">
+        © Copyright 2020
+      </p>
+      <p class="footer__author">
+        Design by
+        <a href="https://fcojavierlpz.netlify.app/" target="__blank" class="footer__link">
+          Francisco J. López
+        </a>
+      </p>
     </div>
   </footer>
 </template>
@@ -47,5 +50,27 @@ export default {};
     font-size: 1.5rem;
   }
 
+  &__author {
+    font-size: 1.6rem;
+    margin: 2rem;
+  }
+
+  &__link {
+    &:link,
+    &:visited {
+      color: $color-grey-light-1;
+      background-color: $color-grey-dark-3;
+      text-decoration: none;
+      display: inline-block;
+      transition: all 0.2s;
+    }
+
+    &:hover,
+    &:active {
+      color: $color-primary;
+      box-shadow: 0 1rem 2rem rgba($color-black, 0.4);
+      transform: scale(1.3);
+    }
+  }
 }
 </style>
